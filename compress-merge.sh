a70c87a8fd02c986df8d35993c16e8e940344d9a
Ray Bogman <ray@jira.nl>
2016-03-26 13:50:46 +0100

    update Readme

---
+++ compress-merge.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#timestamp
+dt=$(date +%Y%m%d%H%M%S)
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# CSS
+cd $DIR/css
+java -jar /usr/share/yui-compressor/yui-compressor.jar style.css -o style.min.css
+java -jar /usr/share/yui-compressor/yui-compressor.jar custom.css -o custom.min.css
+java -jar /usr/share/yui-compressor/yui-compressor.jar magnific.css -o magnific.css.min.css
+java -jar /usr/share/yui-compressor/yui-compressor.jar normalize.css -o normalize.css.min.css
+
+cat *min.css > $dt.css
+
+#JS - is al minified
+cd $DIR/js
+cat *.js > $dt.js
+
+#einde
+
